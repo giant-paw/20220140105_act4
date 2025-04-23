36b9d174d40b713a2cbe64accdb8b71fd3164e9d
giant-paw <giantpawbusiness@gmail.com>
2025-04-23 19:39:29 +0700

    membuat controller dan menggunakan pada inputan

--- lib/order_page.dart
+++ lib/order_page.dart
@@ -8,37 +8,74 @@ class OrderPage extends StatefulWidget {
 }
 
 class _OrderPageState extends State<OrderPage> {
+
+  final TextEditingController makananController = TextEditingController();
+  final TextEditingController minumanController = TextEditingController();
+  final TextEditingController jmlMakananController = TextEditingController();
+  final TextEditingController jmlMinumanController = TextEditingController();
+  final int totalHarga = 0;
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Order Page')),
-      body: Form(child: Column(
-        spacing: 16,
-        children: [
-          
-          Text('MAKANAN'),
+      body: Form(
+        child: Column(
+          spacing: 16,
+          children: [
+            Text('MAKANAN'),
 
-          TextFormField(
-            decoration: const InputDecoration(labelText: 'Pesan Makanan'),
-          ),
-          
-          TextFormField(
-            decoration: const InputDecoration(labelText: 'Jumlah Makanan'),
-          ),
-          
-          Text('MINUMAN'),
+            TextFormField(
+              controller: makananController,
+              decoration: const InputDecoration(labelText: 'Pesan Makanan'),
+              validator: (value) {
+                if(value == null || value.isEmpty) {
+                  return 'Masukkan Pesanan Makanan Anda';
+                }
+                return null;
+              },
+            ),
 
-          TextFormField(
-            decoration: const InputDecoration(labelText: 'Pesan Minuman'),
-          ),
-          
-          TextFormField(
-            decoration: const InputDecoration(labelText: 'Jumlah Minuman'),
-          ),
+            TextFormField(
+              controller: jmlMakananController,
+              decoration: const InputDecoration(labelText: 'Jumlah Makanan'),
+              validator: (value) {
+                if(value == null || value.isEmpty) {
+                  return 'Masukkan Jumlah Makanan';
+                }
+                return null;
+              },
+            ),
 
+            Text('MINUMAN'),
 
-        ],
-      )),
+            TextFormField(
+              controller: minumanController,
+              decoration: const InputDecoration(labelText: 'Pesan Minuman'),
+              validator: (value) {
+                if(value == null || value.isEmpty) {
+                  return 'Masukkan Pesanan Minuman Anda';
+                }
+                return null;
+              },
+            ),
+
+            TextFormField(
+              controller: jmlMinumanController,
+              decoration: const InputDecoration(labelText: 'Jumlah Minuman'),
+              validator: (value) {
+                if(value == null || value.isEmpty) {
+                  return 'Masukkan Jumlah Minuman';
+                }
+                return null;
+              },
+            ),
+
+            ElevatedButton(onPressed: () {},
+             child: Text('Pesan Sekarang'))
+          ],
+        ),
+      ),
     );
   }
 }
